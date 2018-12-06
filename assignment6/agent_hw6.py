da19f5ab5f55fa4b8040c641c4e6889fe9b8bf62
yisomething <32249739+yisomething@users.noreply.github.com>
2018-12-05 22:45:10 -0700

    Add files via upload

--- assignment6/agent_hw6.py
+++ assignment6/agent_hw6.py
@@ -6,7 +6,7 @@
 from rl_glue import BaseAgent
 import numpy as np
 from tile3 import*
-
+import random
 
 class Agent(BaseAgent):
     """
@@ -17,19 +17,16 @@ class Agent(BaseAgent):
 
     def __init__(self):
         """Declare agent variables."""
-        self.epsilon =0
-        self.gamma = 1.0
         self.tilings = 8
-        self.alpha = 0.01/8
+        self.alpha = 0.1/8
         self.iht = IHT(2048)
-        self.shape = [8,8]
-        self.delta=0.9
+        self.sizeTilings = [8,8]
+        self.lmda=0.9
 
         self.last_state = None
         self.last_action = None
         self.last_tile =None
-        self.weights =None
-        self.values =None
+        self.weight =None
         self.Z=None
 
     def agent_init(self):
@@ -40,8 +37,7 @@ class Agent(BaseAgent):
         begins
         """
 
-        self.weights = np.random.uniform(-0.001,0,2048)
-        self.values = np.zeros([self.shape[0],self.shape[1],3])
+        self.weight = np.random.uniform(-0.001,0,2048)
 
     def agent_start(self, state):
         """
@@ -51,26 +47,12 @@ class Agent(BaseAgent):
         a new episode, pick the first action, don't forget about exploring
         starts
         """
-        tile = [self.shape[0]*(state[0]+1.2)/(1.2+0.5),self.shape[1]*(state[1]+0.07)/(0.07+0.07)]
-        #self.action
-        if np.random.uniform()<self.epsilon:
-            self.action = np.random.randint(3)
-        else:
-            Q=[]
-            #c = -float('inf')
-            for self.action in range(3):
-                tiles_action = tiles(self.iht,self.tilings,tile,[self.action])
-                # ignore zero,sum w where not zero 
-                Q.append(np.sum(self.weights[tiles_action]))
-            Q = np.array(Q)
-            self.action =  np.random.choice(np.where(Q == Q.max())[0])
-        
+        self.Z = np.zeros(2048)
+
         self.last_state = state
-        self.last_action = self.action
-        self.last_tile = tile      
-        self.Z = np.zeros(len(self.weights))
-        
-        return self.action
+        self.last_action = self.action(state)
+
+        return self.last_action
 
 
     def agent_step(self, reward, state):
@@ -79,42 +61,23 @@ class Agent(BaseAgent):
         Returns: action - integer
         Hint: select an action based on pi
         """
-        TD_error = reward
-        for i in tiles(self.iht, self.tilings, self.last_tile, [self.last_action]):
-            TD_error -=self.weights[i]
-            self.Z[i]=1.0
-            
-        
-        tile = [self.shape[0]*(state[0]+1.2)/(1.2+0.5),self.shape[1]*(state[1]+0.07)/(0.07+0.07)]
-        
-        if np.random.uniform()<self.epsilon:
-            self.action = np.random.randint(3)
-        else:
-            Q=[]
-            #c = -float('inf')
-            for self.action in range(3):
-                tiles_action = tiles(self.iht,self.tilings,tile,[self.action])
-                # ignore zero,sum w where not zero 
-                Q.append(np.sum(self.weights[tiles_action]))
-            Q = np.array(Q)
-            self.action =  np.random.choice(np.where(Q == Q.max())[0])        
-        
-        activated = np.zeros(len(self.weights))
-        
-        for i in tiles(self.iht, self.tilings, tile, [self.action]):
-            activated[i]=1.0
-            TD_error+=self.gamma*self.weights[i]
-            
-        
-        self.weights += self.alpha*TD_error*self.Z
-        self.Z = self.gamma * self.delta*self.Z
-        self.last_state = state
-        self.last_action = self.action
-        self.last_tile = tile
-        return self.action
 
-        
-        
+        action = self.action(state)
+        delta = reward + self.q_value(state,action) - self.q_value(self.last_state,self.last_action)
+
+        for index in self.get_index(self.last_state,self.last_action):
+            delta = delta - self.weight[index]
+            self.Z[index] = 1
+
+        for index in self.get_index(state,action):
+            delta = delta + self.weight[index]
+
+        self.weight = self.weight + self.alpha*self.Z*delta
+        self.Z = self.lmda * self.Z
+        self.last_state = state
+        self.last_action = action
+
+        return action
 
     def agent_end(self, reward):
         """
@@ -122,16 +85,10 @@ class Agent(BaseAgent):
         Returns: Nothing
         Hint: do necessary steps for policy evaluation and improvement
         """
-        TD_error =reward
-        for i in tiles(self.iht, self.tilings, self.last_tile, [self.last_action]):
-            TD_error -=self.weights[i]
-            self.Z[i]=1.0
-        
-        self.weights+=self.alpha*TD_error*self.Z
-        
-        return
-        
-        
+
+        delta = reward-self.q_value(self.last_state, self.last_action)
+        self.weight = self.weight + self.alpha * self.Z * delta
+
     def agent_message(self, in_message):
         """
         Arguments: in_message - string
@@ -139,3 +96,36 @@ class Agent(BaseAgent):
         This function is complete. You do not need to add code here.
         """
         pass
+
+    def get_index(self,state,action):
+
+        tile = [self.sizeTilings[0]*(state[0])/(1.2+0.5),self.sizeTilings[1]*(state[1])/(0.07+0.07)]
+
+        return tiles(self.iht, self.tilings,tile,[action])
+
+    def q_value(self,state,action):
+
+
+        tile = [self.sizeTilings[0]*(state[0])/(1.2+0.5),self.sizeTilings[1]*(state[1])/(0.07+0.07)]
+
+        X = np.zeros(len(self.weight))
+
+        for i in tiles(self.iht, self.tilings,tile,[action]):
+            X[i] = 1.0
+
+        return np.dot(self.weight,X)
+
+
+    def action(self,state):
+
+        tile = [self.sizeTilings[0]*(state[0])/(1.2+0.5),self.sizeTilings[1]*(state[1])/(0.07+0.07)]
+
+        Q=np.zeros(3)
+        for action in range(3):
+            X = np.zeros(2048)
+            for i in tiles(self.iht, self.tilings,tile,[action]):
+                X[i] = 1.0
+            Q[action] = np.dot(self.weight,X)
+        
+        return random.choice(random.choice(np.nonzero(Q == np.amax(Q))))
+
